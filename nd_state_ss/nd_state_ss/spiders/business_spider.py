a920a0e533bce27f96e79fdf9503a687071c0c3c
Brice Wolfgang <BriceWolfgang@Gmail.com>
2019-12-17 00:21:24 -0500

    Commenting, Cleaning

--- nd_state_ss/nd_state_ss/spiders/business_spider.py
+++ nd_state_ss/nd_state_ss/spiders/business_spider.py
@@ -14,27 +14,30 @@ class XBizSpider(scrapy.Spider):
 							 )
 
 	def parse(self, response):
-		"""For parsing the main list of buissness which is returned by the search reqest"""
+        """For parsing the main list of business which is returned by the search request"""
 
-		# parsing SOURCE_ID's from main list of buissness entites
+        # parsing SOURCE_ID's from main list of business entities
 		dirty_ids = response.selector.re('"ID":\d+')
 		clean_ids = [x.split(':')[1] for x in dirty_ids]
-		res_json = json.loads(response.text) #main list of buissnes to json
+		res_json = json.loads(response.text) #main list of business to json
 		biz_title = {}
 
 		for id in clean_ids:
 			# Store ID & Title
 			biz_title[id] = res_json["rows"][id]['TITLE'][0] 
-			print(res_json["rows"][id]['TITLE'][0] )
+
 			# Request Details
 			yield scrapy.Request(url=f"https://firststop.sos.nd.gov/api/FilingDetail/business/{id}/false",
 		                         callback=self.parse_biz,
 				                 )
+		# Data referred to in .ipynb
 		pickle.dump(biz_title, open('biz_title.p','wb'))
 
 
 	def parse_biz(self, response):
-		"""For parsing indivdual buissness' information"""
+		"""For parsing indivdual buissness' FilingDetail"""
+
+		# xpath expressions 
 		maddy_xp=\
 		"//DRAWER/DRAWER_DETAIL_LIST/DRAWER_DETAIL/LABEL[contains(text(), 'Mailing Address')]//following-sibling::VALUE/text()"
 		owner_xp =\
@@ -47,28 +50,5 @@ class XBizSpider(scrapy.Spider):
 		owner = response.xpath(owner_xp).get()
 		agent = response.xpath(agent_xp).get()
 
-		#
-		# pickle.dump({id:{'maddress': maddress,
-		# 				 'owner': owner,
-		# 				 'agent': agent,
-		# 				 }
-		# 			},
-		# 			open('biz_info.p','ab'))
-
+		# Written to JSON Lines when this is invoked on the command line
 		yield{'id':id,'maddress': maddress,'owner': owner,'agent': agent,}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
